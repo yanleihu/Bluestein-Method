28f4f1675768332a0a4162c5063713f7027ce9a5
yanleihu <62788946+yanleihu@users.noreply.github.com>
2020-09-30 18:34:32 +0800

    Add files via upload

---
+++ Bluestein_dft.m
@@ -0,0 +1,23 @@
+function g=Bluestein_dft(x,f1,f2,fs,mout)
+
+[m,n]=size(x);
+a = -exp(j*2*pi*f1/fs);
+w = exp(-j*2*pi*(f2-f1)/(mout*fs)); 
+h=((-m+1):max(mout-1,m-1)).';
+np=(0:(m-1))';
+wp=w.^((h.^2)./2);
+ap=a.^(-np).*wp(m+np);
+xx=x.*ap(:,ones(1,n));
+nf=2^nextpow2(m+mout-1);
+ftt=fft(1./wp(1:(mout-1+m)),nf);
+ft=fft(xx,nf);
+ft=ft.*ftt(:,ones(1,n));
+g=ifft(ft);
+g=g(m:(m+mout-1),:).*wp(m:(m+mout-1),ones(1,n));
+
+l=linspace(0,mout-1,mout);l=l./mout.*(f2-f1)+f1;
+Mshifty=floor(-n/2); 
+Mshift=repmat(exp(-1i.*2*pi.*l.*Mshifty/fs),[n 1]);
+g=g.'.*Mshift;
+
+end
