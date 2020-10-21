9b6b5e7406b1e9dce1cdff67fd0bf1c0fa910170
yanleihu <62788946+yanleihu@users.noreply.github.com>
2020-10-21 17:09:15 +0800

    Update Bluestein_dft.m

--- Bluestein_dft.m
+++ Bluestein_dft.m
@@ -1,8 +1,10 @@
 function g=Bluestein_dft(x,f1,f2,fs,mout)
 
 [m,n]=size(x);
-a = -exp(j*2*pi*f1/fs);
-w = exp(-j*2*pi*(f2-f1)/(mout*fs)); 
+f11=f1+(mout*fs+f2-f1)/(2*mout);
+f22=f2+(mout*fs+f2-f1)/(2*mout);
+a = exp(j*2*pi*f11/fs);
+w = exp(-j*2*pi*(f22-f11)/(mout*fs)); 
 h=((-m+1):max(mout-1,m-1)).';
 np=(0:(m-1))';
 wp=w.^((h.^2)./2);
@@ -15,7 +17,7 @@ ft=ft.*ftt(:,ones(1,n));
 g=ifft(ft);
 g=g(m:(m+mout-1),:).*wp(m:(m+mout-1),ones(1,n));
 
-l=linspace(0,mout-1,mout);l=l./mout.*(f2-f1)+f1;
+l=linspace(0,mout-1,mout);l=l./mout.*(f22-f11)+f11;
 Mshift=-m/2; 
 Mshift=repmat(exp(-1i.*2*pi.*l.*(Mshift+1/2)/fs),[n 1]);
 g=g.'.*Mshift;
