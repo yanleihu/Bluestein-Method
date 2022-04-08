253d9567bf9a77ee1c81a68ee59a6583918c81dd
yanleihu <62788946+yanleihu@users.noreply.github.com>
2022-04-08 18:46:08 +0800

    Bluestein_dft.m

--- Bluestein_dft.m
+++ Bluestein_dft.m
@@ -1,25 +1,22 @@
-function g=Bluestein_dft(x,f1,f2,fs,mout)
+function b=Bluestein_dft(x,f1,f2,fs,mout)
 
 [m,n]=size(x);
 f11=f1+(mout*fs+f2-f1)/(2*mout);
 f22=f2+(mout*fs+f2-f1)/(2*mout);
 a = exp(j*2*pi*f11/fs);
 w = exp(-j*2*pi*(f22-f11)/(mout*fs)); 
-h=((-m+1):max(mout-1,m-1)).';
-np=(0:(m-1))';
-wp=w.^((h.^2)./2);
-ap=a.^(-np).*wp(m+np);
-xx=x.*ap(:,ones(1,n));
-nf=2^nextpow2(m+mout-1);
-ftt=fft(1./wp(1:(m+mout-1)),nf);
-ft=fft(xx,nf);
-ft=ft.*ftt(:,ones(1,n));
-g=ifft(ft);
-g=g(m:(m+mout-1),:).*wp(m:(m+mout-1),ones(1,n));
-
-l=linspace(0,mout-1,mout);l=l./mout.*(f22-f11)+f11;
+h=(-m+1):max(mout-1,m-1);
+mp=m+mout-1;
+h=w.^((h.^2)./2);
+ft=fft(1./h(1:mp),2^nextpow2(mp));
+b=a.^(-(0:(m-1))).*h(m:(2*m-1));
+b=fft(x.*repmat(b.',[1 n]),2^nextpow2(mp));
+b=ifft(b.*repmat(ft.',[1 n]));
+b=(b(m:mp,1:n)).'.*repmat((h(m:mp)),[n 1]);
+l=linspace(0,mout-1,mout);
+l=l./mout.*(f22-f11)+f11;
 Mshift=-m/2; 
 Mshift=repmat(exp(-1i.*2*pi.*l.*(Mshift+1/2)/fs),[n 1]);
-g=g.'.*Mshift;
+b=b.*Mshift;
 
 end
